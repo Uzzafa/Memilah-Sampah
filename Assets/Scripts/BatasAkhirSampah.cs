adc013c802925634ec1f8c3b4c7f41a0ffaf6a59
Fauza <naufalfauza70@gmail.com>
2022-09-23 15:50:52 +0700

    Memilah_Sampah
    Memilah Sampah_Naufal Fauza Pratama_02120147287

---
+++ Assets/Scripts/BatasAkhirSampah.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BatasAkhirSampah : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        SceneManager.LoadScene("GameOver"); // Baris Ini akan digunakan pada submodul GameOver
+    }
+}
